e921cd570da1e33761a8dfe39548e6dffd3547a8
jilyu <jeremy_leano@dlsu.edu.ph>
2026-06-02 02:12:36 +0800

    feat: setup prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [],
+  providers: [PrismaModule],
 })
 export class AppModule {}
