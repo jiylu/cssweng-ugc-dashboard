68547ca21264b07565867de336c9d19ef16d9a6f
DaniDev937 <daniel_rebudiao@dlsu.edu.ph>
2026-06-06 14:25:29 +0800

    Feat: login UI design

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -2,6 +2,7 @@
 // Local
 import Image from 'next/image';
 import logo from './../public/Logo.svg'
+import logo2 from './../public/Logo-notext-lime.svg'
 import styles from './../ui/loginRegisterStyles/login.module.css';
 
 // React
@@ -67,8 +68,24 @@ export default function Page() {
       <title>Log-in</title>
       <main>
         <section className={styles.overlay} >
+          {/* LEFT PANEL */}
+          <div className={styles.leftpanel}>
+            <div className={styles.gridlines} />
+            <div className={styles.orb1} />
+            <div className={styles.orb2} />
+
+            <Image src={logo} alt="Logo" className="w-[200px] mb-10"/>
+
+            <div className={styles.herotext}>
+              <h2>Everything you need,<br /><span className="text-[#AAFB79]">in one place.</span></h2>
+              <p>Manage your content, track analytics,<br />and collaborate with clients.</p>
+            </div>
+          </div>
+
+          {/* RIGHT PANEL */}
           <div className={styles.loginpanel} >
-            <Image src={logo} alt="Logo" className="mb-25"/>
+            <Image src={logo2} alt="Logo2" className="w-[50px]"/>
+            <h1 className={styles.h1text}>Welcome Back,</h1>
             <div className={styles.cardwrapper}>
               <Card className="w-full">
                 <CardHeader>
