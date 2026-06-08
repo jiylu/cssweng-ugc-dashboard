e6bbafcb83f55f6219fcaa21f6528f05ac8eb540
jilyu <jeremy_leano@dlsu.edu.ph>
2026-06-08 20:07:12 +0800

    feat: added create proposal service, schema, and tests

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,16 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './features/users/users.module';
 import { SupabaseModule } from './supabase/supabase.module';
 import { CampaignsModule } from './features/campaigns/campaigns.module';
+import { ProposalsModule } from './features/proposals/proposals.module';
 
 @Module({
-  imports: [SupabaseModule, PrismaModule, UsersModule, CampaignsModule],
+  imports: [
+    SupabaseModule,
+    PrismaModule,
+    UsersModule,
+    CampaignsModule,
+    ProposalsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/features/campaigns/campaigns.module.ts
+++ backend/src/features/campaigns/campaigns.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CampaignsService } from './campaigns.service';
 import { CampaignsController } from './campaigns.controller';
-import { UserService } from '../users/users.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [CampaignsService, UserService],
+  imports: [PrismaModule, UsersModule],
+  providers: [CampaignsService],
   controllers: [CampaignsController],
 })
 export class CampaignsModule {}
