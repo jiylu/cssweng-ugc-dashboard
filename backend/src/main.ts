1637ea0d1b4ecabd11cf5823441fe838d841ab76
jilyu <jeremy_leano@dlsu.edu.ph>
2026-06-09 23:20:01 +0800

    fix: cors issue by enabling cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,11 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
