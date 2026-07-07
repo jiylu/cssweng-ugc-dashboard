5669bd2ef0792ee2c1c10feb79c04d90ea4e3100
jilyu <jeremy_leano@dlsu.edu.ph>
2026-07-08 02:45:00 +0800

    chore: added docs for notifications api

--- backend/src/features/notifications/notifications.controller.ts
+++ backend/src/features/notifications/notifications.controller.ts
@@ -1,21 +1,29 @@
 import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { FindNotificationsQueryDTO } from './dto/find-notifications-query.dto';
+import {
+  ApiFindNotification,
+  ApiFindNotificationsForUser,
+  ApiMarkNotificationAsRead,
+} from './docs/notifications.controller.swagger';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
+  @ApiFindNotification()
   @Get(':notificationId')
   findOne(@Param('notificationId') notificationId: string) {
     return this.notificationsService.findNotification(notificationId);
   }
 
+  @ApiFindNotificationsForUser()
   @Get()
   findMany(@Query() query: FindNotificationsQueryDTO) {
     return this.notificationsService.findNotificationsForUser(query);
   }
 
+  @ApiMarkNotificationAsRead()
   @Post('read-notification/:notificationId')
   markRead(@Param('notificationId') notificationId: string) {
     return this.notificationsService.markAsRead(notificationId);
