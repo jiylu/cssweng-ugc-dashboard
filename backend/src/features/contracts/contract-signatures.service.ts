da5bf914815296b233bdc6745b9f8b1fd051d1b8
jilyu <jeremy_leano@dlsu.edu.ph>
2026-08-10 18:09:02 +0800

    feat: implement service for storing signatures

---
+++ backend/src/features/contracts/contract-signatures.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from 'src/shared/prisma/prisma.service';
+import { ContractsService } from './contracts.service';
+import { StoreSignatureDTO } from './dto/store-signature.dto';
+
+@Injectable()
+export class ContractSignaturesService {
+  private readonly logger = new Logger(ContractSignaturesService.name);
+  constructor(
+    private prisma: PrismaService,
+    private contractService: ContractsService,
+  ) {}
+
+  async storeSignature(dto: StoreSignatureDTO) {
+    this.logger.debug(
+      `Storing ${dto.signerRole} signature for contract ${dto.contractId}`,
+    );
+
+    const contract = await this.contractService.findContractByUID(
+      dto.contractId,
+    );
+
+    const storedSignature = await this.prisma.contractSignatures.create({
+      data: {
+        contract_id: contract.contract_id,
+        signer_role: dto.signerRole,
+        signature_url: dto.signatureURL,
+        initials_url: dto.initialsURL,
+        signed_at: new Date(),
+      },
+    });
+
+    return storedSignature;
+  }
+}
